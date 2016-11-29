0b79050d2571c712d255a1510990e02ca7e6232d
samueltcsantos <thiagsamuel@gmail.com>
2016-11-29 18:55:02 -0300

    Added initial fake dashboard data.

--- assets/app/dashboard/dashboard.js
+++ assets/app/dashboard/dashboard.js
@@ -3,4 +3,29 @@
  */
 app.controller("Dashboard", ['$scope','$timeout', 'Rest', function($scope, $timeout, Rest) {
 
+  var myChart = Highcharts.chart('chartUserVsActivity', {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Frequency of activity creation by users'
+    },
+    xAxis: {
+      categories: ['Samuel Santos', 'Luana Santos', 'Ludmila Guedes' , 'Maisa Lins']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      }
+    },
+    series: [{
+      name: 'Activity',
+      data: [1, 1, 4, 8]
+    }, {
+      name: 'It',
+      data: [5, 7, 3, 9]
+    }]
+  });
+
+
 }]);
