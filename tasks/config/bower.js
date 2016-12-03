84e203e7f6ea843da664461d856338fd7ac9ecb9
samueltcsantos <thiagsamuel@gmail.com>
2016-12-03 00:11:56 -0300

    Moving bower dependencies after npm install

--- tasks/config/bower.js
+++ tasks/config/bower.js
@@ -2,6 +2,8 @@
  * Copyright @ 2016 by Samuel T. C. Santos, All rights reserved.
  */
 
+var path = require('path');
+
 /**
  * Task to pull out specific files from bower packages.
  */
@@ -9,10 +11,19 @@ module.exports = function (grunt) {
   grunt.config.set('bower', {
     install: {
       options: {
-        layout: function(type, component) {
-          return type;
+        layout: function(type, component, source) {
+
+          var renamedType = type;
+
+          if (type == 'js') {
+            renamedType = "javascripts";
+          }
+          else if (type == 'css'){
+            renamedType = "stylesheets"
+          }
+          return path.join(component, renamedType);
         },
-        targetDir: './assets',
+        targetDir: './assets/resources',
         install: true,
         cleanTargetDir: false,
         cleanBowerDir: false
